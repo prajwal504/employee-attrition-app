aaed06c2c00f5716b8f4f4a79b4de545b817cd77
M S Prajwal <msprajwal302@gmail.com>
2026-04-19 19:09:51 +0530

    Create 4_About.py

---
+++ 4_About.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+# ✅ FIRST line
+st.set_page_config(layout="wide")
+
+st.title("📘 About This Project")
+
+st.markdown("""
+### 💼 HR AI System
+
+This platform helps HR teams:
+
+- Predict employee attrition using Machine Learning  
+- Analyze workforce trends  
+- Screen resumes automatically  
+
+### 🧠 Technologies Used
+- Python  
+- Streamlit  
+- Random Forest  
+
+### 🎯 Goal
+To help companies make smarter, data-driven HR decisions.
+""")
