9f6075f8b1609d2183d475561181cdb008700734
M S Prajwal <msprajwal302@gmail.com>
2026-04-19 19:12:35 +0530

    Update 3_Resume_Screening.py

--- 3_Resume_Screening.py
+++ 3_Resume_Screening.py
@@ -1,6 +1,9 @@
+import streamlit as st
+st.set_page_config(page_title="Resume Screening", layout="wide")
+
 st.title("📄 Resume Screening")
 
-file = st.file_uploader("Upload Resume", type=["txt"])
+file = st.file_uploader("Upload Resume (.txt)", type=["txt"])
 
 skills = ["python", "sql", "machine learning", "data analysis"]
 
@@ -17,6 +20,6 @@ if file:
     st.progress(score * 25)
 
     if score >= 3:
-        st.success("Strong Candidate")
+        st.success("Strong Candidate 💪")
     else:
-        st.warning("Needs Improvement")
+        st.warning("Needs Improvement ⚠️")
